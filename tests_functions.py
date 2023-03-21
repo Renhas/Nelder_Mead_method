6bdd812a202fb74bfe8cf119dac657f006258610
Hasan <emin.oren.139@gmail.com>
2023-03-21 18:02:03 +0300

    Initial commit

---
+++ tests_functions.py
@@ -0,0 +1,55 @@
+import pytest
+import sympy as sm
+
+
+variables = sm.symbols("x1:10")
+
+class TestBaseFunction:
+    @pytest.mark.parametrize(
+        ("expression", "expr_var"), [
+            (sm.sin(variables[0] + sm.cos(variables[1])), {variables[0], variables[1]}),
+
+        ]
+    )
+    def test_create(self, expression, expr_var):
+        func = BaseFunction(expression, expr_var)
+        assert func.expr == expression
+        assert func.dimension == len(expr_var)
+        assert func.variables == expr_var
+
+    @pytest.mark.parametrize(
+        ("expression", "expr_var",  "test_input", "test_output"), [
+            (2.0*sm.ln(variables[0]), [variables[0]], [sm.E], 2.0)
+        ]
+
+    )
+    def test_calculate(self, expression, expr_var, test_input, test_output):
+        func = BaseFunction(expression, expr_var)
+        assert func.calulate(test_input) == test_output
+
+
+class TestPolynomial:
+    @pytest.mark.parametrize(
+        ("coefficients", "view", "variable_count"), [
+            ([[2, 3], [3, 4]],
+             2.0 * variables[0] + 3.0 * variables[0] ** 2 + 3.0 * variables[1] + 4.0 * variables[1] ** 2,
+             2)
+        ]
+    )
+    def test_create(self, coefficients, view, variable_count):
+        poly = Polynomial(coefficients)
+        assert poly.expr == view
+        assert poly.dimension == variable_count
+
+    @pytest.mark.parametrize(
+        ("coefficients", "test_input", "test_output"), [
+            ([[2, 3], [3, 4]], [0, 0], 0.0),
+            ([[2, 3], [3, 4]], [1, 2], 27.0),
+            ([[1], [2, 1]], [1, 1], 4.0)
+        ]
+    )
+    def test_calculate(self, coefficients, test_input, test_output):
+        poly = Polynomial(coefficients)
+        assert poly.calculate(test_input) == test_output
+
+
