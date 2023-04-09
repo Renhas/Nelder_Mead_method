5c6d332022d252687146b49d53f699cb05c3b5aa
Hasan <emin.oren.139@gmail.com>
2023-04-09 19:52:35 +0300

    Fix pylint errors... OMG

--- tests/test_nm.py
+++ tests/test_nm.py
@@ -195,7 +195,7 @@ class TestSimplex:
         assert sim.good == three_points[-2]
         assert sim.worst == three_points[-1]
 
-    # pylint: disable=too-many-instance-attributes
+    # pylint: disable=too-many-arguments
     @pytest.mark.parametrize(
         ("function", "points", "index", "new_point", "expected"), [
             (Rosenbroke(), [(Point(0, 0), 1), (Point(1, 1), 0), (Point(0, 1), 101)],
@@ -203,7 +203,6 @@ class TestSimplex:
              ((Point(1, 1), 0), (Point(0.5, 0.5**2), 0.25), (Point(0, 0), 1)))
         ]
     )
-    # pylint: disable=too-many-instance-attributes
     def test_replace(self, function: BaseFunction, points: list,
                      index: int, new_point: Point, expected: tuple):
         """Тестирование замещения точки
